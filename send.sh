eae0174a6d8588e51e546f25e58df46799af3c9f
Aakash Parikh <aparikh98@users.noreply.github.com>
2018-12-11 19:31:50 -0800

    Update send.sh

--- send.sh
+++ send.sh
@@ -3,4 +3,4 @@ echo 'Running image recognition'
 ./complete_demo.py
 echo 'Sending to remote'
 scp output.txt ee106a-ach@cory.eecs.berkeley.edu:~/ros_workspaces/lab7/src/planning/src/
-#nT6cK+nA
+#replace with your lab login
